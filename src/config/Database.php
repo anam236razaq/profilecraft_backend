9f9e70f6fc436f3358fcf80c1a4465c90d309a85
Anam Razaq <anamrazaq236@gmail.com>
2026-05-25 08:37:59 +0500

    fix:ssl mode

--- src/config/Database.php
+++ src/config/Database.php
@@ -34,8 +34,6 @@ class Database {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
-            // Aiven SSL configuration
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         ];
 
         try {
