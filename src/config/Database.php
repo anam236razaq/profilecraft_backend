3fd76cb881b63337e13fd7d258c90eb92f7b185c
Anam Razaq <anamrazaq236@gmail.com>
2026-05-26 08:23:30 +0500

    update:db file

--- src/config/Database.php
+++ src/config/Database.php
@@ -34,6 +34,8 @@ class Database {
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::MYSQL_ATTR_SSL_CA => $_ENV['DB_SSL_CA'] ?? null,
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         ];
 
         try {
